d671b6f52e283a540bf25db7188672122e18794b
Vaneesha19 <117992444+Vaneesha19@users.noreply.github.com>
2024-04-15 15:33:42 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+public class prog1{
+public static void main(String args[])
+{
+	int a=5,b=10,c=2;
+	if(a>b){
+		if(a>c){
+		System.out.println("Largest number is"+a);
+		}
+	else{
+		System.out.println("Largest Number is"+c);
+	}
+	}
+	else{
+		System.out.println("largest number is"+b);
+	}
+}}
